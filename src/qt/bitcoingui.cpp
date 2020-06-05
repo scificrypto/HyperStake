013d79d712546a82219c291acacdb9e34d04ed9d
flapmin <nerdlabs001@gmail.com>
2020-06-05 15:26:40 -0400

    wallet size

--- src/qt/bitcoingui.cpp
+++ src/qt/bitcoingui.cpp
@@ -93,9 +93,9 @@ BitcoinGUI::BitcoinGUI(const NetworkStyle * networkStyle, QWidget *parent):
     notificator(0),
     rpcConsole(0)
 {
-    setMinimumSize(800,800);
-    setMaximumSize(800,800);
-    resize(800, 800);
+    setFixedSize(850,700);
+//    setMaximumSize(750,750);
+//    resize(800, 800);
     
     QString windowTitle = tr("HyperStake") + " - " + tr("Wallet");
     windowTitle += " " + networkStyle->getTitleAddText();
