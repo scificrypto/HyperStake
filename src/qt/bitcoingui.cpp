3f5db9e09cd6b00ae3da739724c0a8b0045c638e
anthonyrjwood <anthonyrjwood@gmail.com>
2021-10-01 19:19:26 +0100

    progressBar UI fix

--- src/qt/bitcoingui.cpp
+++ src/qt/bitcoingui.cpp
@@ -696,7 +696,7 @@ void BitcoinGUI::setNumBlocks(int count, int nTotalBlocks)
             progressBar->setMaximum(nTotalBlocks);
             progressBar->setValue(count);
             progressBar->setVisible(true);
-            progressBar->setStyleSheet("QProgressBar { background-color: grey; border: 0px solid grey; border-radius: 0px; padding: 0px; text-align: center; color: white; width: 588px; margin-right: 5px; } QProgressBar::chunk { background: QLinearGradient(x1: 0, y1: 0, x2: 1, y2: 0, stop: 0 #3ddc97, stop: 1 #1351d8); border-radius: 0px; margin: 0px; }");
+            progressBar->setStyleSheet("QProgressBar { background-color: grey; border: 0px solid grey; border-radius: 0px; padding: 0px; text-align: center; color: white; width: 550px; } QProgressBar::chunk { background: QLinearGradient(x1: 0, y1: 0, x2: 1, y2: 0, stop: 0 #3ddc97, stop: 1 #1351d8); border-radius: 0px; margin: 0px; }");
         }
 
         tooltip = tr("Downloaded %1 of %2 blocks of transaction history (%3% done).").arg(count).arg(nTotalBlocks).arg(nPercentageDone, 0, 'f', 2);
